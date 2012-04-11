3819a965932606677adb800ff9988c680e8771b3
Reid Draper <reiddraper@gmail.com>
2012-04-10 22:30:12 -0400

    More helper funcs

--- src/crosscram/core.clj
+++ src/crosscram/core.clj
@@ -1,7 +1,7 @@
 (ns crosscram.core
-  (:use [clojure.core.match :only [match]]))
+  (:require [clojure.core.match :as match]))
 
-(defn abs [x]
+(defn- abs [x]
   (if (< x 0)
     (* -1 x)
     x))
@@ -14,22 +14,40 @@
 (defn horizontal-pair? [[a b] [c d]] 
   (if (= a c)
     (= 1 (abs (- b d)))
-    false
-    ))
+    false))
 
 (defn vertical-pair? [[a b] [c d]] 
   (if (= b d)
-    (= 1 (abs (- a c)))
-    false
-    ))
+    (= 1 (abs (- a 0)))
+    false))
+
+(defn- generate-horizontal-for-row [columns row]
+  (take (- columns 1)
+        (for [x (range columns)]
+          [[row x] [row  (+ 1 x)]])))
+
+(defn- generate-vertical-for-column [rows column]
+  (take (- rows 1)
+        (for [x (range rows)]
+          [[x column] [(+ 1 x) column]])))
+
+(defn generate-horizontal [rows columns]
+  (apply concat
+         (map (partial generate-horizontal-for-row columns)
+              (range rows))))
+
+(defn generate-vertical [rows columns]
+  (apply concat
+         (map (partial generate-vertical-for-column rows)
+              (range columns))))
 
 (defn- two-nil? [coll]
   (= true (reduce (fn [acc elem]
-                    (match [acc elem]
-                           [true _] true
-                           [0 nil] 1
-                           [1 nil] true
-                           [_ _] 0))
+                    (match/match [acc elem]
+                                 [true _] true
+                                 [0 nil] 1
+                                 [1 nil] true
+                                 [_ _] 0))
                   0 coll)))
 
 (defn can-play-horizontal? [board]
@@ -38,5 +56,10 @@
 
 (defn can-play-vertical? [board]
   (can-play-horizontal? (apply map vector board)))
+
+(defn board [rows columns]
+  (vec (repeat rows
+               (vec
+                 (take 5 (repeat nil))))))
 
 (defn add-piece)
