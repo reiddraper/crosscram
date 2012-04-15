290cfa712eb419c75086da2c264a9b9a73c201fb
Reid Draper <reiddraper@gmail.com>
2012-04-15 18:23:40 -0400

    Fix bug in play-symmetrical winner detection

--- src/crosscram/core.clj
+++ src/crosscram/core.clj
@@ -135,6 +135,18 @@
       {:winner :draw :rounds (- max-games-before-draw to-go)}
       (let [g1 (winner (play game bot-a bot-b))
             g2 (winner (play game bot-b bot-a))]
-        (if (= g1 g2)
-          {:winner g1 :rounds (+ 1 (- max-games-before-draw to-go))}
-          (recur (dec to-go)))))))
+        (match/match [g1 g2]
+                     [:horizontal :vertical] {:winner :bot-a :rounds (+ 1 (- max-games-before-draw to-go))}
+                     [:vertical :horizontal] {:winner :bot-b :rounds (+ 1 (- max-games-before-draw to-go))}
+                     [:horizontal :horizontal] (recur (dec to-go))
+                     [:vertical :vertical] (recur (dec to-go))
+                     ;; TODO: not sure why,
+                     ;; by eliminating the two lines
+                     ;; above this comment
+                     ;; and replacing them with the
+                     ;; line below throws an exception:
+                     ;; java.lang.RuntimeException: java.lang.Exception: No match found.
+                     ;; Followed 1 branches. Breadcrumbs:
+                     ;;
+                     ;;[_ _] (recur (dec to-go))
+                     )))))
