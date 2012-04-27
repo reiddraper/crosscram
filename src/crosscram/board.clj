474e2a1992d02d53cba828bb67799b44cd840a8e
Neil Traft <ntraft@gmail.com>
2012-04-26 21:51:57 -0400

    Introduced a convenience function that generates available moves. This made the bots trivially simple.

--- src/crosscram/board.clj
+++ src/crosscram/board.clj
@@ -1,6 +1,10 @@
 (ns crosscram.board
   (:require [clojure.core.match :as match]))
 
+;;
+;; Validation Functions
+;;
+
 (defn valid-pair? [[a b] [c d]] 
   (cond (= a c) (= 1 (Math/abs (- b d)))
         (= b d) (= 1 (Math/abs (- a c)))
@@ -15,24 +19,6 @@
   (if (= b d)
     (= 1 (Math/abs (- a c)))
     false))
-
-(defn- generate-horizontal-for-row [columns row]
-  (take (- columns 1)
-        (for [x (range columns)]
-          [[row x] [row  (+ 1 x)]])))
-
-(defn- generate-vertical-for-column [rows column]
-  (take (- rows 1)
-        (for [x (range rows)]
-          [[x column] [(+ 1 x) column]])))
-
-(defn generate-horizontal [rows columns]
-  (mapcat (partial generate-horizontal-for-row columns)
-          (range rows)))
-
-(defn generate-vertical [rows columns]
-  (mapcat (partial generate-vertical-for-column rows)
-          (range columns)))
 
 (defn- two-nil? [coll]
   (= true (reduce (fn [acc elem]
@@ -50,6 +36,10 @@
 (defn can-play-vertical? [board]
   (can-play-horizontal? (apply map vector board)))
 
+;;
+;; Convenience functions for determining possible moves
+;;
+
 (defn two-d-get [coll [a b]]
   (get (get coll a) b))
 
@@ -57,15 +47,37 @@
   (and (nil? (two-d-get board pos-a))
        (nil? (two-d-get board pos-b))))
 
-(defn available-horizontal [game]
-  (filter
-    (fn [arg] (location-empty? (:board game) (first arg) (last arg)))
-    (generate-horizontal (:rows game) (:columns game))))
+(defn- generate-horizontal-for-row [columns row]
+  (take (- columns 1)
+        (for [x (range columns)]
+          [[row x] [row  (+ 1 x)]])))
 
-(defn available-vertical [game]
+(defn- generate-vertical-for-column [rows column]
+  (take (- rows 1)
+        (for [x (range rows)]
+          [[x column] [(+ 1 x) column]])))
+
+(defn- generate-horizontal [rows columns]
+  (mapcat (partial generate-horizontal-for-row columns)
+          (range rows)))
+
+(defn- generate-vertical [rows columns]
+  (mapcat (partial generate-vertical-for-column rows)
+          (range columns)))
+
+(defn generate-possible-moves [game]
+  (match/match (:next-player game)
+                :horizontal (generate-horizontal (:rows game) (:columns game))
+                :vertical (generate-vertical (:rows game) (:columns game))))
+
+(defn available-moves [game]
   (filter
     (fn [arg] (location-empty? (:board game) (first arg) (last arg)))
-    (generate-vertical (:rows game) (:columns game))))
+    (generate-possible-moves game)))
+
+;;
+;; Board-update functions
+;;
 
 (defn board [rows columns]
   (vec (repeat rows
--- src/crosscram/bots.clj
+++ src/crosscram/bots.clj
@@ -2,36 +2,19 @@
   (:require [crosscram.core :as crosscram])
   (:require [crosscram.board :as board])
   (:require [clojure.core.match :as match])
-  (:require [clojure.set :as set])
-  (:require clojure.pprint)
   )
 
 (defn brute-force-moves [game]
-  (let [rows (:rows game)
-        columns (:columns game)]
-    (loop [moves (match/match (:next-player game)
-                              :horizontal (board/generate-horizontal rows columns)
-                              :vertical (board/generate-vertical rows columns))]
-      (if (apply board/location-empty? (:board game) (first moves))
-        (first moves)
-        (recur (rest moves))))))
+  (first (board/available-moves game)))
 
 (defn random-moves [game]
-  (let [rows (:rows game)
-        columns (:columns game)]
-    (loop [moves (match/match (:next-player game)
-                              :horizontal (board/generate-horizontal rows columns)
-                              :vertical (board/generate-vertical rows columns))]
-      (let [m (set/difference (set moves) (set (map :move (:history game))))
-            random-move (rand-nth (vec m))]
-        (if (apply board/location-empty? (:board game) random-move)
-          random-move
-          (recur (remove #{random-move} m)))))))
+  (rand-nth (board/available-moves game)))
 
 
-(defn -main [& args]
-  (println (crosscram.core/play-symmetric
-    (apply crosscram.core/new-game args)
-    brute-force-moves
-    random-moves
-    100)))
+(defn -main [rows columns num-games]
+  (println (str "Scores: "
+    (crosscram/play-symmetric
+      (crosscram/new-game (Integer/parseInt rows) (Integer/parseInt columns) :horizontal )
+      brute-force-moves
+      random-moves
+      (Integer/parseInt num-games)))))
