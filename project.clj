cbff651c4a3a88d98f78d9fc436de04be09cad5c
Tim McCormack <cortex@brainonfire.net>
2012-05-23 18:32:26 -0400

    reindent project file

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject crosscram "0.0.1-SNAPSHOT"
-	:description "crosscram game"
-	:dependencies [[org.clojure/clojure "1.3.0"]
-					[org.clojure/core.match "0.2.0-alpha9"]]
-	:main crosscram.bots)
+  :description "crosscram game"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/core.match "0.2.0-alpha9"]]
+  :main crosscram.bots)
