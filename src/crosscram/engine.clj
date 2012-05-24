385ea197cfdce6e783ac865e311e19f6d7b1b3b8
Tim McCormack <cortex@brainonfire.net>
2012-05-23 20:23:07 -0400

    Basic definitions of gameplay

---
+++ src/crosscram/engine.clj
@@ -0,0 +1,9 @@
+(ns crosscram.engine
+  "Game engine")
+
+;; A bot is given a game object (as defined in crosscram.game)
+;; A bot returns a domino
+
+
+;; TODO(timmc:2012-05-23) Decide on tournament rules for bots throwing
+;; exceptions, returning nil, returning bad dominoes...
