8e38486e0e28e3171fe33f6100550b4829b9a510
Tim McCormack <cortex@brainonfire.net>
2012-05-24 17:50:04 -0400

    Doc: Dominoes *may* be horizontal, but player is vertical. Also:
    * A "square" is just a coordinate, not a box containing data.
    * Stronger def of "move ordinal" (may want renaming)

--- src/crosscram/engine.clj
+++ src/crosscram/engine.clj
@@ -1,7 +1,12 @@
 (ns crosscram.engine
   "Game engine.
 
-A bot is a function of [game -> domino].")
+A bot is a function of [game -> domino].
+
+The returned domino must be vertical; that is, the row coordinates differ
+by 1 but the column coordinates are equal. That is, every player plays
+vertical, and sees the other player as playing horizontal. (The game engine
+gives the second player a transposed view of the board.)")
 
 ;; TODO(timmc:2012-05-23) Decide on tournament rules for bots throwing
 ;; exceptions, returning nil, returning bad dominoes...
--- src/crosscram/game.clj
+++ src/crosscram/game.clj
@@ -4,18 +4,17 @@
 In this documentation, \"vector\" means any indexed, sequential collection.
 
 A board is a 2-dimensional matrix. Create with #'board, access with
-#'location-empty? Addressed by row, then column.
+#'location-empty?
 
-A square on the board is addressed by a vector of [row column], zero-indexed.
+A cell on the board is addressed by a vector of [row column], zero-indexed.
+These may also be called coordinates or squares.
 
-A domino is a vector of two squares: [[r0 c] [r1 c]]. Note that the
-first coordinate (row) always differs by 1 between the two squares, but
-the column coordinate is always the same. That is, every player always
-thinks they are playing vertical. (The game engine arranges for this.)
+A domino is a vector of two squares: [[r0 c0] [r1 c1]].
 The order of the squares in a vector is not important, but the game engine
 will not alter it.
 
-A game history is a vector of dominoes in order of play.
+A game history is a vector of dominoes in order of play. The index of each
+domino in the vector is the ordinal of the move that placed it.
 
 The board contains an alternate view of history. Each cell contains either
 the ordinal (from the history vector) of the move that covered that square,
