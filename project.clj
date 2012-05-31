c2c024dd49d8bfba75961609264ba8f717e16cf3
Tim McCormack <cortex@brainonfire.net>
2012-05-31 14:45:42 -0400

    Provide repo url; indicate license version.

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject crosscram "0.0.1-SNAPSHOT"
   :description "crosscram game"
+  :url "https://github.com/baznex/crosscram"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/core.match "0.2.0-alpha9"]]
-  :license  {:name "Eclipse Public License"
+  :license  {:name "Eclipse Public License v1.0"
              :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main ^:skip-aot crosscram.main)
