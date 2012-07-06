bb19880d2cc147c86f9c8cb273b8a44054549514
Tim McCormack <cortex@brainonfire.net>
2012-07-05 23:01:42 -0400

    Fix engine doc -- we're actually using horizontal dominoes!

--- src/crosscram/engine.clj
+++ src/crosscram/engine.clj
@@ -3,9 +3,9 @@
 
 A bot is a function of [game -> domino].
 
-The returned domino must be vertical; that is, the row coordinates differ
-by 1 but the column coordinates are equal. That is, every player plays
-vertical, and sees the other player as playing horizontal. (The game engine
+The returned domino must be horizontal; that is, the column coordinates differ
+by 1 but the row coordinates are equal. That is, every player plays
+horizontal, and sees the other player as playing vertical. (The game engine
 gives the second player a transposed view of the board.)"
 
   (:require [crosscram.game :as game]))
@@ -13,7 +13,7 @@ gives the second player a transposed view of the board.)"
 ;; TODO(timmc:2012-05-23) Decide on tournament rules for bots throwing
 ;; exceptions, returning nil, returning bad dominoes...
 
-;; TODO(timme:2012-05-24) Wait, how would we even decide which player won a
+;; TODO(timmc:2012-05-24) Wait, how would we even decide which player won a
 ;; 3-player game? Last player to place a tile before someone fails, or last
 ;; player standing after successive elimination?
 
