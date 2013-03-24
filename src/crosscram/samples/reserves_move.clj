770d1787b177a392d401f0ee2ce3f55344767a3b
Reid Draper <reiddraper@gmail.com>
2013-03-23 20:49:47 -0700

    Refactor shared bits of *-saves?

--- src/crosscram/samples/reserves_move.clj
+++ src/crosscram/samples/reserves_move.clj
@@ -3,21 +3,23 @@
   move later"
   (:require [crosscram.game :as game]))
 
+(defn could-move?
+  [game altered-row-1 altered-row-2 c1 c2]
+  (and (game/valid-move? (:board game) [[altered-row-1 c1] [altered-row-1 c2]])
+       (not (game/valid-move?
+              (:board game) [[altered-row-2 c1] [altered-row-2 c2]]))))
+
 (defn above-saves?
   [game [[r1 c1] [r2 c2]]]
   (let [above-row (dec r1)
         above-above-row (- r1 2)]
-    (and (game/valid-move? (:board game) [[above-row c1] [above-row c2]])
-         (not (game/valid-move?
-               (:board game) [[above-above-row c1] [above-above-row c2]])))))
+    (could-move? game above-row above-above-row c1 c2)))
 
 (defn below-saves?
   [game [[r1 c1] [r2 c2]]]
   (let [below-row (inc r1)
         below-below-row (+ r1 2)]
-    (and (game/valid-move? (:board game) [[below-row c1] [below-row c2]])
-         (not (game/valid-move?
-               (:board game) [[below-below-row c1] [below-below-row c2]])))))
+    (could-move? game below-row below-below-row c1 c2)))
 
 (defn reserves-move?
   "Return true if this
